f6ed268649a744e1dfd8b64f67d8ae89a91d7916
Maria Vanessa Moreno <MariaVanessaMoreno@outlook.com>
2019-11-14 15:05:07 -0500

    new school content

---
+++ src/org/launchcode/java/demos/java4python/school/Course.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos.java4python.school;
+
+public class Course {
+    //make 3 fields
+    //is the access level private or public?
+    //static or final?
+    //what's the data type? (String, int, double)
+    //make one an ArrayList or HashMap
+    //use Student class
+}
---
+++ src/org/launchcode/java/demos/java4python/school/Student.java
@@ -0,0 +1,36 @@
+package org.launchcode.java.demos.java4python.school;
+
+public class Student {
+
+    private static int nextStudentId = 1;
+    private String name;
+    private final int studentId;
+    private int numberOfCredits;
+    private double gpa;
+
+    public Student(String name, int studentId,
+                   int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+}
